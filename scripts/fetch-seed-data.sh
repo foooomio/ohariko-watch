f2ed23f6324a7614bfa34d7aeb697bbef8361abb
foooomio <foooomio@gmail.com>
2026-06-20 17:50:46 +0900

    Update data (#13)

--- scripts/fetch-seed-data.sh
+++ scripts/fetch-seed-data.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 url='https://sgrkb.github.io/shigariko_button/ex/ohariko.html'
 regexp='https://x.com/Shigariko_/status/\d+'
 
-curl -fsSL "$url" | grep -o -E "$regexp" | grep -v '1803381639843860835'
+curl -fsSL "$url" | grep -o -E "$regexp" | sort \
+  | grep -v -e 1803381639843860835 -e 1818531106796585218 -e 1842850368012906651
