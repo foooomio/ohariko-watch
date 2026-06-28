37cd6a454fb18f1e0f30d04758dc1a7058059a83
foooomio <foooomio@gmail.com>
2026-06-29 00:39:27 +0900

    Add TodaysPost

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { Layout } from "@/components/Layout";
+import { TodaysPost } from "@/components/TodaysPost";
 import { Summary } from "@/components/Summary";
 import { PostingTimeScatter } from "@/components/PostingTimeScatter";
 import { PostingTimeHistogram } from "@/components/PostingTimeHistogram";
@@ -26,6 +27,7 @@ export function Home() {
 
   return (
     <Layout lastUpdatedAt={generatedAt}>
+      <TodaysPost latestPost={posts.at(-1)} />
       <Summary posts={posts} streaks={streaks} sortedStreaks={sortedStreaks} />
       <PostingTimeScatter posts={posts} />
       <PostingTimeHistogram posts={posts} />
